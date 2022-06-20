6aee21d7920697281a73de107f4266858aaa51f0
David Pashley <david@codingcraft.co.uk>
2022-06-20 10:16:43 +0200

    Fix error on login
    We were missing a parameter to the wp_login method. This adds it.

--- public/CaptureEmailFromLogin.php
+++ public/CaptureEmailFromLogin.php
@@ -23,18 +23,19 @@ final class CaptureEmailFromLogin {
 	public static function load( BlueOdinLoader $loader, BlueOdinSession $session ): self
 	{
 		$capture_email_from_login = new CaptureEmailFromLogin( $session );
-		$loader->add_action( 'wp_login', $capture_email_from_login, 'action_wp_login' );
+		$loader->add_action( 'wp_login', $capture_email_from_login, 'action_wp_login', 10, 2 );
 		$loader->add_action( 'wp_loaded', $capture_email_from_login, 'action_wp_loaded' );
 
 		return $capture_email_from_login;
 	}
 
 	/**
+	 * @param string $user_login
 	 * @param WP_User $user
 	 *
 	 * @return void
 	 */
-	public function action_wp_login( WP_User $user ): void
+	public function action_wp_login( string $user_login, WP_User $user ): void
 	{
 		$this->session->set_email( $user->user_email, 'logged_in_user' );
 	}
